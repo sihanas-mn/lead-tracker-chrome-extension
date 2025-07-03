49b4b44b09f47666af9130e081478864fba61369
sihanas <iamsihanas@gmail.com>
2025-07-03 16:31:13 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-let myLeads = ["www.freeleads.com", "megaleads.com", "pastlives.com"]
-const inputEl = document.getElementById("input-el")
-const saveInputEl = document.getElementById("input-btn")
-const ulEl = document.getElementById("unorderedList-el")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const saveInputEl = document.getElementById("input-btn");
+const ulEl = document.getElementById("unorderedList-el");
 
-saveInputEl.addEventListener ("click", function() {
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
-})
+saveInputEl.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
+});
 
-for (i = 0; i < myLeads.length; i++) {
-    ulEl.textContent += myLeads[i]
+function renderLeads() {
+    let listItems = "";
+
+  for (i = 0; i < myLeads.length; i++) {
+    listItems += `<li><a href='${myLeads[i]}' target='_blank'>  ${myLeads[i]} </a></li>`
+  }
+  console.log(listItems);
+  ulEl.innerHTML = listItems;
 }
