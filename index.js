c49ffaa777924abb1ff61f5b8c406fbb05944096
sihanas <iamsihanas@gmail.com>
2025-07-03 21:01:50 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const inputEl = document.getElementById("input-el");
 const saveInputEl = document.getElementById("input-btn");
 const ulEl = document.getElementById("unorderedList-el");
 
+let leadsFromLocal = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocal) {
+  myLeads = leadsFromLocal
+  renderLeads()
+}
+
 saveInputEl.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
   renderLeads();
 });
 
@@ -15,6 +23,5 @@ function renderLeads() {
   for (i = 0; i < myLeads.length; i++) {
     listItems += `<li><a href='${myLeads[i]}' target='_blank'>  ${myLeads[i]} </a></li>`
   }
-  console.log(listItems);
   ulEl.innerHTML = listItems;
 }
